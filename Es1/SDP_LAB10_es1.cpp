3c607ca913561dce998922af3928884838545363
Francesco Longo <france193.longo@studenti.polito.it>
2018-06-10 20:36:09 +0200

    completed

--- Es1/SDP_LAB10_es1.cpp
+++ Es1/SDP_LAB10_es1.cpp
@@ -36,47 +36,53 @@
 *
 **/
 
+// !UNICODE
 #ifndef UNICODE
 #define UNICODE
 #define _UNICODE
-#endif // !UNICODE
+#endif
 
+// !_CRT_SECURE_NO_WARNINGS
 #ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
-#endif // !_CRT_SECURE_NO_WARNINGS
+#endif 
 
+// include
 #include <Windows.h>
 #include <tchar.h>
+#include <assert.h>
 
-typedef struct _DATA_STRUCT_T {
+// typedef
+typedef struct {
 	LPTCH filename; // input
-	UINT length;	// output lenght array
-	LPUINT vet;		// output ordered array
+	UINT length; // output
+	LPUINT vet; // output
 } DATA_STRUCT_T;
 
 typedef DATA_STRUCT_T* LPDATA_STRUCT_T;
 
-// PROTOTYPES
+// prototypes
 DWORD WINAPI sortFile(LPVOID param);
-LPWSTR getErrorMessageAsString(DWORD errorCode);
-int Return(int seconds, int value);
+VOID removeHandleAndUpdateRealIndexes(LPHANDLE handles, LPDWORD realIndexes, LPDWORD pnThreads, DWORD indexToRemove);
+VOID merge(LPUINT a, UINT a_len, LPUINT b, UINT b_len, LPUINT res);
 
-// MAIN
+// main
 INT _tmain(INT argc, LPTSTR argv[]) {
 	LPHANDLE handles;
 	LPDATA_STRUCT_T data;
 	UINT i, j;
-	UINT nThreads;
-	LPUINT arr, indexes;
-	UINT totSize;
-	LPBOOL endedArray;
-	INT minTh;
+	DWORD nThreads;
+	LPUINT arr, arr2;
+	UINT totSize, totSize2;
 	HANDLE hOut;
 	DWORD nOut;
+	DWORD whoEnded_a, whoEnded_b;
+	LPDWORD realIndexes;
 
+	// check input file
 	if (argc < 3) {
-		_ftprintf(stderr, _T("Usage: %s <list_of_input_files> <output_file>\n"), argv[0]);
-		return Return(5, 1);
+		_ftprintf(stderr, _T("Usage: %s list_of_input_files output_file\n"), argv[0]);
+		return 1;
 	}
 
 	// first parameter is the executable, the last one is the output file
@@ -85,151 +91,137 @@ INT _tmain(INT argc, LPTSTR argv[]) {
 	// allocate the array of handles for the threads
 	handles = (LPHANDLE)malloc((nThreads) * sizeof(HANDLE));
 
+	// allocate the array of real indexes where to find the data of a given thread
+	realIndexes = (LPDWORD)malloc((nThreads) * sizeof(DWORD));
+
 	// and allocate the array of structures for data sharing with the threads
 	data = (LPDATA_STRUCT_T)malloc((nThreads) * sizeof(DATA_STRUCT_T));
 
-	// create threads
+	//check allocation
+	if (handles == NULL || realIndexes == NULL || data == NULL) {
+		_ftprintf(stderr, _T("Impossible to allocate some data\n"));
+		return 1;
+	}
+
+	// create the threads
 	for (i = 0; i < nThreads; i++) {
 		// i is the index of the thread, while j is the corresponding index in the program arguments
 		j = i + 1;
 
-		// copy into the structure the respective filename
+		// copy into the structure the pointer to the file name
 		data[i].filename = argv[j];
 
-		// launch the thread
-		handles[i] = CreateThread(0,	// security attribute: default
-			0,							// stack size: default
-			sortFile,					// function for the thread
-			(LPVOID)&data[i],			// argument for the thread (pointer to void)
-			0,							// creation flag: default
-			NULL						// where to save thread id just created
-		);
+		// and initialize the realIndex (currently the data is in the right position
+		realIndexes[i] = i;
 
-		// check the handle value for the thread just created
+		// and launch the thread
+		handles[i] = CreateThread(0, 0, sortFile, (LPVOID)&data[i], 0, NULL);
+
+		// check the handle value
 		if (handles[i] == INVALID_HANDLE_VALUE) {
 			// if unable to create a thread, better to terminate immediately the process
 			_ftprintf(stderr, _T("Impossible to create thread %d\n"), j);
 			// the return statement on the main will exit the current process (every thread)
-			return Return(5, 1);
+			return 2;
 		}
 	}
 
-	// wait for the end of all threads also for an undefinite time 
-	WaitForMultipleObjects(nThreads, handles, TRUE, INFINITE);
+	// wait for the first thread to end
+	whoEnded_a = WaitForMultipleObjects(nThreads, handles, FALSE, INFINITE);
 
-	// release resources for all threads
-	for (i = 0; i < nThreads; i++) {
-		CloseHandle(handles[i]);
-	}
+	// to obtain which thread it is terminated
+	whoEnded_a -= WAIT_OBJECT_0;
 
-	// deallocate array of threads handles
-	free(handles);
+	// read how may values
+	totSize = data[realIndexes[whoEnded_a]].length;
 
-	// preliminary phase to merge the results of the threads
-	totSize = 0;
-
-	// allocate an array that will contain for each thread the current index used in the merge
-	indexes = (LPUINT)malloc(nThreads * sizeof(UINT));
-
-	// this array will contain for each thread information related to the fact that we already took all the elements from his results
-	endedArray = (LPBOOL)malloc(nThreads * sizeof(BOOL));
-
-	// initialize these support variables
-	for (i = 0; i < nThreads; i++) {
-		// totSize contains the total number of elements
-		totSize += data[i].length;
-
-		// the starting index for the thread is 0 (no values have been read yed)
-		indexes[i] = 0;
-
-		// check on the number of elements (if a thread had some issues reading the file, the lenght was set to 0)
-		if (data[i].length != 0) {
-			// array with some data, can read from it
-			endedArray[i] = FALSE;
-		}
-		else {
-			// array is empty, this flag is set to FALSE not to read from the array
-			endedArray[i] = TRUE;
-		}
-	}
-
-	// allocate the final array for the results of the merge
+	// allocate an array
 	arr = (LPUINT)malloc(totSize * sizeof(UINT));
 
-	for (i = 0; i < totSize; i++) {
-		// minTh contains the index of the thread that currently has the minimum value (candidate for being stored into the results)
-		minTh = -1;
+	// and copy to it the content of the ended thread
+	memcpy(arr, data[realIndexes[whoEnded_a]].vet, totSize * sizeof(UINT));
 
-		// scan all the results of the threads
-		for (j = 0; j < nThreads; j++) {
-
-			// if i can read from this result set
-			if (!endedArray[j]) {
-
-				// i check if in the current position it contains a lower value with respect to the memorized thread
-				if (minTh == -1 || data[j].vet[indexes[j]] < data[minTh].vet[indexes[minTh]]) {
-					// if this is true, or if no thread has been considered yet, i memorize the index of this thread
-					minTh = j;
-				}
-			}
-		}
-
-		// i move into the results the value (that is the current minimum among the values that i can still use
-		arr[i] = data[minTh].vet[indexes[minTh]];
-
-		// update the index of the thread that provided the value
-		indexes[minTh]++;
-
-		// and check if it can still provide values
-		if (!(indexes[minTh] < data[minTh].length)) {
-			// if i reached the end of its result set, i turn the flag to TRUE so that in next iterations i won't use it anymore
-			endedArray[minTh] = TRUE;
-
-			// and free the array (i won't use it anymore)
-			free(data[minTh].vet);
-		}
+	// free the array of the results of the thread (only if it has been allocated)
+	if (data[realIndexes[whoEnded_a]].length > 0) {
+		free(data[realIndexes[whoEnded_a]].vet);
 	}
 
+	// remove the handle from the array and play with indexes to redirect them
+	removeHandleAndUpdateRealIndexes(handles, realIndexes, &nThreads, whoEnded_a);
+
+	// while there are still some threads, wait for them
+	while (nThreads > 0) {
+		// wait for a thread
+		whoEnded_b = WaitForMultipleObjects(nThreads, handles, FALSE, INFINITE);
+		whoEnded_b -= WAIT_OBJECT_0;
+
+		// sum up the size of the actual array and the results of the ended thread
+		totSize2 = totSize + data[realIndexes[whoEnded_b]].length;
+
+		// allocate a new array
+		arr2 = (LPUINT)malloc(totSize2 * sizeof(UINT));
+
+		// merge the actual array and results of the thread in the new array
+		merge(arr, totSize, data[realIndexes[whoEnded_b]].vet, data[realIndexes[whoEnded_b]].length, arr2);
+
+		// free the old array
+		free(arr);
+
+		// update the pointer: now arr refers to the new array
+		arr = arr2;
+
+		// update the total size
+		totSize = totSize2;
+
+		// free the results array of the ended thread
+		if (data[realIndexes[whoEnded_b]].length > 0) {
+			free(data[realIndexes[whoEnded_b]].vet);
+		}
+
+		// remove the handle from the array and play with indexes to redirect them
+		removeHandleAndUpdateRealIndexes(handles, realIndexes, &nThreads, whoEnded_b);
+	}
+
+	// release some resources
+	free(handles);
+	free(realIndexes);
+	free(data);
+
+	// print the resulting array
 	_tprintf(_T("Sorted array: \n"));
 	for (i = 0; i < totSize; i++) {
 		_tprintf(_T("%d "), arr[i]);
 	}
 	_tprintf(_T("\n"));
 
-	// release resources no more needed
-	free(data);
-	free(indexes);
-	free(endedArray);
-
-	// now write the output file
+	// now comes the output file
 	hOut = CreateFile(argv[argc - 1], GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
 	// check the handle
 	if (hOut == INVALID_HANDLE_VALUE) {
-		_tprintf(_T("Unable to create file %s. Error: %s\n"), argv[argc - 1], getErrorMessageAsString(GetLastError()));
+		_tprintf(_T("Unable to create file %s. Error: %x\n"), argv[argc - 1], GetLastError());
 	}
 
 	// write the number of integers
 	if (!WriteFile(hOut, &totSize, sizeof(totSize), &nOut, NULL) || nOut != sizeof(totSize)) {
-		_ftprintf(stderr, _T("Error writing the total nunmber of integers. Error: %s\n"), getErrorMessageAsString(GetLastError()));
+		_ftprintf(stderr, _T("Error writing the total nunmber of integers. Error: %x\n"), GetLastError());
 		free(arr);
 		CloseHandle(hOut);
-		return Return(5, 2);
+		return 3;
 	}
 
 	// write the integers
 	if (!WriteFile(hOut, arr, totSize * sizeof(UINT), &nOut, NULL) || nOut != totSize * sizeof(UINT)) {
-		_ftprintf(stderr, _T("Error writing the integers. Error: %s\n"), getErrorMessageAsString(GetLastError()));
+		_ftprintf(stderr, _T("Error writing the integers. Error: %x\n"), GetLastError());
 		free(arr);
 		CloseHandle(hOut);
-		return Return(5, 3);
+		return 4;
 	}
 
 	_tprintf(_T("Written correctly to output file %s\n"), argv[argc - 1]);
-
 	free(arr);
 	CloseHandle(hOut);
 
-	return Return(5, 0);
+	return 0;
 }
 
 // the output of this function is stored inside the structure passed as a parameter
@@ -241,28 +233,22 @@ DWORD WINAPI sortFile(LPVOID param) {
 	LPUINT vet;
 
 	// open the binary file for reading
-	hIn = CreateFile(data->filename,
-		GENERIC_READ,
-		FILE_SHARE_READ,
-		NULL,
-		OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL,
-		NULL);
+	hIn = CreateFile(data->filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
 
 	// check the HANDLE value
 	if (hIn == INVALID_HANDLE_VALUE) {
-		_ftprintf(stderr, _T("Cannot open input file %s. Error: %s\n"), data->filename, getErrorMessageAsString(GetLastError()));
+		_ftprintf(stderr, _T("Cannot open input file %s. Error: %x\n"), data->filename, GetLastError());
 		// set the length to 0 so the main won't read the array
 		data->length = 0;
-		return Return(5, 0);
+		return 0;
 	}
 
 	// read dimension (is the first integer inside the file)
 	if (!ReadFile(hIn, &n, sizeof(n), &nRead, NULL) || nRead != sizeof(n)) {
-		_ftprintf(stderr, _T("Error reading the number of integers in file %s. Error: %s\n"), data->filename, getErrorMessageAsString(GetLastError()));
+		_ftprintf(stderr, _T("Error reading the number of integers in file %s. Error: %x\n"), data->filename, GetLastError());
 		data->length = 0;
 		// set the length to 0 so the main won't read the array
-		return Return(5, 0);
+		return 0;
 	}
 
 	// allocate array of proper size
@@ -271,10 +257,10 @@ DWORD WINAPI sortFile(LPVOID param) {
 	// then i read all the values
 	for (UINT i = 0; i < n; i++) {
 		if (!ReadFile(hIn, &vet[i], sizeof(n), &nRead, NULL) || nRead != sizeof(n)) {
-			_ftprintf(stderr, _T("Error reading the integers in file %s. In this file there should be %u of them. Error: %s\n"), data->filename, n, getErrorMessageAsString(GetLastError()));
+			_ftprintf(stderr, _T("Error reading the integers in file %s. In this file there should be %u of them. Error: %x\n"), data->filename, n, GetLastError());
 			data->length = 0;
 			free(vet);
-			return Return(5, 0);
+			return 0;
 		}
 	}
 
@@ -300,24 +286,56 @@ DWORD WINAPI sortFile(LPVOID param) {
 	CloseHandle(hIn);
 
 	// then return 0
-	return Return(5, 0);
+	_tprintf(_T("Thread on file %s done\n"), data->filename);
+
+	return 0;
 }
 
-int Return(int seconds, int value) {
-	Sleep(seconds * 1000);
-	return value;
+// this function removes the specified handle specified as index, and updates the realIndexes array that contains
+// the original positions of the structures used by threads inside the data array. They can't be moved because the threads
+// still need to modify them.
+// each thread uses his data passed through a pointer to the element in the array data, and the main uses data[realIndexes[i]]
+// to access the data of the i-th element of the compacted array (corresponding to the realIndexes[i] element in the original array
+VOID removeHandleAndUpdateRealIndexes(LPHANDLE handles, LPDWORD realIndexes, LPDWORD pnThreads, DWORD indexToRemove) {
+	// can close the handle to this thread
+	CloseHandle(handles[indexToRemove]);
+
+	if (*pnThreads > 0) {
+		// decrease the number of threads
+		(*pnThreads)--;
+
+		// if the ended thread is not the last one, take the last element and put in the position of the ended one
+		if (indexToRemove != *pnThreads) {
+			// overwrite the handle
+			handles[indexToRemove] = handles[*pnThreads];
+			// and say: the real position (index) of the data of this thread will be where this thread was in the original array
+			realIndexes[indexToRemove] = realIndexes[*pnThreads];
+		}
+	}
 }
 
-LPWSTR getErrorMessageAsString(DWORD errorCode) {
-	LPWSTR errString = NULL;
-
-	FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
-		0,
-		errorCode,
-		0,
-		(LPWSTR)&errString,
-		0,
-		0);
-
-	return errString;
+// merge 2 ordered array in a unique ordered one 
+VOID merge(LPUINT a, UINT a_len, LPUINT b, UINT b_len, LPUINT res) {
+	DWORD i, j, k;
+	for (i = 0, j = 0, k = 0; i < a_len + b_len; i++) {
+		if (j == a_len) {
+			// the a array ended
+			assert(k < b_len);
+			res[i] = b[k];
+			k++;
+		} else if (k == b_len) {
+			// the b array ended
+			assert(j < a_len);
+			res[i] = a[j];
+			j++;
+		} else if (a[j] < b[k]) {
+			assert(j < a_len);
+			res[i] = a[j];
+			j++;
+		} else {
+			assert(k < b_len);
+			res[i] = b[k];
+			k++;
+		}
+	}
 }
